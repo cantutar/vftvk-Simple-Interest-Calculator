473d2e926e7e275c5ff48261a8548907f6fec252
cantutar <vahitcant@gmail.com>
2021-11-29 06:11:21 +0300

    finished

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function compute() {
   p = document.getElementById("principal").value;
   r = document.getElementById("rate").value;
+  document.getElementById("rate_display").innerHTML = r;
   t = document.getElementById("years").value;
 
   sum = p * r * t;
